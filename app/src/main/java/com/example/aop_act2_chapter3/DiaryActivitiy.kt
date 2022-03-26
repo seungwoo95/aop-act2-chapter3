b29e2a32ae0a27d1fd81a950f6895eb8c95407c2
seungwoo95 <58511384+seungwoo95@users.noreply.github.com>
2022-03-26 20:46:13 +0900

    chapter3 commit

---
+++ app/src/main/java/com/example/aop_act2_chapter3/DiaryActivitiy.kt
@@ -0,0 +1,42 @@
+package com.example.aop_act2_chapter3
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
+
+class DiaryActivitiy: AppCompatActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+
+        val diaryEditText = findViewById<EditText>(R.id.diaryEditText)
+
+        diaryEditText.setText(detailPreferences.getString("detail",""))
+
+
+
+        val runnable = Runnable {
+            getSharedPreferences("diary",Context.MODE_PRIVATE).edit {
+                putString("detail",diaryEditText.text.toString())
+            }
+        }
+
+        diaryEditText.addTextChangedListener {
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 500)
+        }
+    }
+
+
+
+}
